ec3e362dab8d154afb0651677d50cffebf0c3c2b
tnkmt <35386637+tnkmt@users.noreply.github.com>
2018-07-06 18:47:42 +0300

    Update brute.py

--- brute.py
+++ brute.py
@@ -1,6 +1,5 @@
 #
-# Copyright (c) 2017 tnkmt
-#
+# Copyright (c) 2018 tnkmt
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
